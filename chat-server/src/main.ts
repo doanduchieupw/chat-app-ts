f7ca9a10a898670f79109db5bc6d006eb2a6f9fa
doanduchieupw <doanduchieu.pw@Doans-MacBook-Pro.local>
2022-12-05 16:38:43 +0700

    set up auth module

--- chat-server/src/main.ts
+++ chat-server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  const { PORT } = process.env;
+  try {
+    await app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
 }
 bootstrap();
